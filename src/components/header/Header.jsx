3092cfb6849e2744075f8488aa5fae33f95ba269
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-06-12 19:02:42 +0530

    added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,19 +18,29 @@ function Header() {
         {
             name: "order",
             slug: "/order"
+        },
+        {
+            name: "signup",
+            slug: "/signup"
+        },
+        {
+            name: "signin",
+            slug: "/signin"
         }
     ]
 
     return (
+        <header className='bg-gray-600 text-white'>
         <div className='flex '>
             {
                 navItem.map((item) => (
                     <div key={item.slug} className='mr-8'>
-                        <NavLink to={item.slug}>{item.name}</NavLink>
+                        <NavLink to={item.slug} >{item.name}</NavLink>
                     </div>
                 ))
             }
         </div>
+        </header>
     )
 }
 
