ce5bf65cf6d9384de40bc3629a7f657b91b34041
TAMANNA <tamannakarthol320@gmail.com>
2024-06-14 23:28:32 -0700

    added

--- src/components/Singup.jsx
+++ src/components/Singup.jsx
@@ -2,14 +2,31 @@ import React from 'react'
 import Input from './Input'
 import Button from './Button'
 import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { Formik,Form,Field,ErrorMessage} from 'formik'
+
+
+
+
+const signupSchema=Yup.object({
+  name:Yup.string().required(),
+  email:Yup.string().required(),
+  pass:Yup.string().required().min("","enter 2 characters"),
+  mobile:Yup.number().required().min("10","enter valid mobile")
+})
 
 
 function Singup() {
-  const { register, handleSubmit } = useForm();
+  // const { register, handleSubmit } = useForm();
 
   return (
     <>
+      <Formik  
       
+      initialValues={{name:"",email:"",mobile:"",password:"",role:""}}
+    validationSchema={signupSchema}
+      
+      >
 <div className=" flex items-center justify-center w-full py-5">
 <div className='mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-5 border border-black/10'>
 
@@ -18,50 +35,38 @@ function Singup() {
         <p className='mt-2 text-center text-base text-black/60'>
           Already have account?</p>
 
-          <form className='mt-1 flex justify-evenly item-center'>
+          <Form className='mt-1 flex justify-evenly item-center'>
           <div className='space-y-5'>
-            <Input
-              label='Name: '
-              placeholder='Enter your name'
-              type='text'
-              {...register('name', {
-                required: true,
-              })}
-            /> 
+            <label htmlFor="">name</label>
+            <Field type ="text" placeholder="Enter your name" 
+            name="name"/>
+            <ErrorMessage name='name'/>
+              <br /><br />
 
-             <Input
-              label='Email: '
-              placeholder='Enter your Email'
-              type='email'
-              {...register('Email', {
-                required: true,
-              })}
-            />
 
-             <Input
-              label='Mobile: '
-              placeholder='Enter your Mobile Number'
-              type='tel'
-              {...register('Mobile', {
-                required: true,
-              })}
-            />
+              <label htmlFor="">email</label>
+            <Field type ="email" placeholder="Enter your email" name="email"/>
+            <ErrorMessage name='name'/>
+              <br /><br />
 
-            <Input
-              label='Password: '
-              type='password'
-              placeholder='Enter your password'
-              {...register('Password', {
-                required: true,
-              })}
-/>
-            <Button type='submit' className='w-full bg-blue-300 rounded-xl'>
+              <label htmlFor="">mobile</label>
+            <Field type ="tel" placeholder="Enter your mobile
+          " name="mobile"/>
+            <ErrorMessage name='mobile'/>
+              <br /><br />
+
+              <label htmlFor="">password</label>
+            <Field type ="password" placeholder="Enter your password" name="pass"/>
+            <ErrorMessage name='pass'/>
+              <br /><br />
+            <button className='w-full bg-blue-300 rounded-xl'>
               Sign up
-            </Button>
+            </button>
           </div>
-        </form>
+        </Form>
 </div>
 </div>
+</Formik>
 
     </>
   )
