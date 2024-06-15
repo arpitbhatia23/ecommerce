2a062dd119c16da70ab8619e78b3d9df9f7bf9cd
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-06-15 11:56:21 +0530

    added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import Logout from './Logout'
+import Logo from '../Logo'
+import Searchbar from '../Searchbar'
 
 function Header() { 
     const authStautus=useSelector(state=>state.auth.status)
@@ -45,11 +47,13 @@ function Header() {
     ]
 
     return (
-        <header className='bg-gray-600 text-white'>
-        <div className='flex '>
+        <header className='bg-gray-600 text-white w-full  h-12 flex  items-center justify-center'>
+        <div className='flex  '>
+            <Logo classname="text-center "/>
+            <Searchbar/>
             {
                 navItem.map((item) => item.active?(
-                    <div key={item.slug} className='mr-8'>
+                    <div key={item.slug} className='ml-8 mr-8'>
                         <NavLink to={item.slug} >{item.name}</NavLink>
                     </div>
                 ):null)
