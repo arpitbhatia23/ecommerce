08e6fd3cbb2ab3d070412104ce444795bb0fb1a8
TAMANNA <tamannakarthol320@gmail.com>
2024-06-24 22:47:47 -0700

    added

--- src/Admin/AddProduct.jsx
+++ src/Admin/AddProduct.jsx
@@ -1,26 +1,57 @@
 import React from 'react'
 import Input from '../components/Input'
 import Button from '../components/Button'
+import { useForm } from 'react-hook-form'
+import UseEcommerceApi from '../ecommerceApi\'s/API'
+
 
 const AddProduct = () => {
+  
+    const {createProduct}=UseEcommerceApi()
+    const { register, handleSubmit,formState:{errors} } = useForm();
+
+function AddProduct(){
+   createProduct()
+   .then((data)=>{
+    console.log(data)
+
+   })
+
+}
+
   return (
     <div className='flex  gap-x-6'>
     <div className='flex items-center justify-center  w-full py-20'>
       <div className=' w-full max-w-lg bg-gray-100 rounded-xl p-4 border border-black/10 shadow-md shadow-gray-700 '>
-      <form className=''>
-        <Input label="name"/>
+      <form className=''onSubmit={handleSubmit(AddProduct)}>
+        <Input label="name"
+                {...register('name', {
+                  required: true })}/>
         <div className='flex items-center gap-2 mt-2'>
-        <Input label="price" className="w-20 h-12" type="number"/>
-        <Input label="stock" className="w-20 h-12"type="number"/>
+        <Input label="price" className="w-20 h-12" type="number"
+        {...register('price', {
+          required: true })}/>
+
+        <Input label="stock" className="w-20 h-12"type="number"
+                {...register('stock', {
+                  required: true })}/>
         </div>
-        <Input label="category" className=""/>
+        <Input label="category" className=""
+                {...register('category', {
+                  required: true })}/>
         <div className='flex items-center gap-2 mt-2'>
-        <Input label="main Image"type="file" className=""/>
-        <Input label="sub Image" type="file" className="" />
+        <Input label="mainImage"type="file" className=""
+                {...register('mainImage', {
+                  required: true })}/>
+        <Input label="subImage" type="file" className="" 
+                {...register('subImage', {
+                  required: true })}/>
         </div>
         <label >discription</label>
-        <textarea className='w-full h-40 p-2 border border-black/10 rounded-md'/>
-        <Button className='w-full'>Submit</Button>
+        <textarea className='w-full h-40 p-2 border border-black/10 rounded-md'
+                {...register('description', {
+                  required: true })}/>
+        <Button className='w-full' type='submit'>Submit</Button>
       </form>
       </div>
     </div>
